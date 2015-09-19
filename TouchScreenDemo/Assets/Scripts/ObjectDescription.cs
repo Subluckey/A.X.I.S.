d589f72ed6751437b314bba8d1e347a040b29182
Subluckey <Subluckey@gmail.com>
2015-09-19 15:35:47 +0800

    [demo::touch_screen_demo] add boundary
    add boundary

---
+++ TouchScreenDemo/Assets/Scripts/ObjectDescription.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDescription : MonoBehaviour {
+
+	public string id;
+	public string description;
+
+	// Use this for initialization
+	void Start () {
+		id = "";
+		description = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
